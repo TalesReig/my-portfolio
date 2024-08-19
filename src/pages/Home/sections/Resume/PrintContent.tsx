c2e3754d7e4009d018c62c497e668535e6f719fe
Tales Reig Cordova <talesreig03@gmail.com>
2024-08-19 00:08:18 -0300

    usando html oculto

---
+++ src/pages/Home/sections/Resume/PrintContent.tsx
@@ -0,0 +1,78 @@
+import { ForwardedRef, forwardRef } from "react";
+import { Container, Typography, styled } from "@mui/material";
+
+const StyledResume = styled("div")(({ }) => ({
+  backgroundColor: "#f5f5f5",
+  padding: "50px 0",
+  minHeight: "100vh",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+}));
+
+const Section = styled("section")(({ }) => ({  
+  backgroundColor: "#f5f5f5",
+  maxWidth: "800px",
+  width: "100%",
+}));
+
+const PrintContent = forwardRef<HTMLDivElement>((_, ref: ForwardedRef<HTMLDivElement>) => (
+  <StyledResume ref={ref}>
+    <Container maxWidth="md">
+      <Typography variant="h3" color="primary" gutterBottom textAlign="center">
+        Currículo
+      </Typography>
+
+      <br />
+      <br />
+
+      <Section>
+        <Typography variant="h4" color="textPrimary" gutterBottom>
+          Experiência Profissional
+        </Typography>
+        <Typography variant="body1" color="textSecondary" paragraph>
+          <strong>Analista e Desenvolvedor</strong> - NDD
+          <br />
+          <em>Outubro de 2022 - Presente</em>
+          <br />
+          Responsabilidades:
+          <ul>
+            <li>Desenvolvimento de aplicações web utilizando C# e Angular.</li>
+            <li>Design e implementação de APIs RESTful.</li>
+            <li>Colaboração com equipes multidisciplinares para entrega de soluções eficientes.</li>
+          </ul>
+          Conquistas:
+          <ul>
+            <li>Contribuí para a melhoria do processo de Code Review no time, implementando o uso do SonarQube nos projetos.</li>
+          </ul>
+        </Typography>
+      </Section>
+
+      <Section>
+        <Typography variant="h4" color="textPrimary" gutterBottom>
+          Formação Acadêmica
+        </Typography>
+        <Typography variant="body1" color="textSecondary" paragraph>
+          <strong>Graduação em Ciência da Computação</strong> - IFSC
+          <br />
+          <em>2020 - 2024</em>
+          <br />
+        </Typography>
+      </Section>
+
+      <Section>
+        <Typography variant="h4" color="textPrimary" gutterBottom>
+          Cursos Relevantes
+        </Typography>
+        <Typography variant="body1" color="textSecondary" paragraph>
+          <strong>Academia do Programador</strong>
+          <br />
+          <em>2021 - 2022</em>
+          <br />
+        </Typography>
+      </Section>
+    </Container>
+  </StyledResume>
+));
+
+export default PrintContent;
--- src/pages/Home/sections/Resume/Resume.tsx
+++ src/pages/Home/sections/Resume/Resume.tsx
@@ -1,94 +1,59 @@
-import { Container, Typography, Button, styled } from "@mui/material";
+import { Container, Button, styled } from "@mui/material";
 import { Download as DownloadIcon } from "@mui/icons-material";
-import FileDownloader from "../../../../utils/FileDownloader";
+import React from "react";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
+import PrintContent from "./PrintContent";
 
-const StyledResume = styled("div")(({ }) => ({
+const AllContainer = styled("div")(({ }) => ({
   backgroundColor: "#f5f5f5",
-  padding: "50px 0",
-  minHeight: "100vh",
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-}));
-
-const Section = styled("section")(({ }) => ({
-  marginBottom: "40px",
-  maxWidth: "800px",
-  width: "100%",
+  paddingBottom: "70px",
 }));
 
 function Resume() {
-  const handleDownload = () => {
-    const downloader = new FileDownloader("/my-portfolio/Curriculo.pdf", "Currículo_Tales_Reig.pdf");
-    downloader.download();
+  const printContentRef = React.useRef<HTMLDivElement>(null);
+
+  const handlePrint = () => {
+    if (printContentRef.current) {
+      html2canvas(printContentRef.current).then((canvas) => {
+        const imgData = canvas.toDataURL("image/png");
+        const pdf = new jsPDF();
+        const imgWidth = 210; // A4 width in mm
+        const pageHeight = 295; // A4 height in mm
+        const imgHeight = (canvas.height * imgWidth) / canvas.width;
+        let heightLeft = imgHeight;
+
+        let position = 0;
+
+        pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+
+        while (heightLeft >= 0) {
+          position -= pageHeight;
+          pdf.addPage();
+          pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+          heightLeft -= pageHeight;
+        }
+
+        pdf.save("Curriculo_Tales_Reig.pdf");
+      });
+    }
   };
 
   return (
-    <StyledResume>
-      <Container maxWidth="md">
-        <Typography variant="h3" color="primary" gutterBottom textAlign="center">
-          Currículo
-        </Typography>
-
-        <br />
-        <br />
-
-        <Section>
-        <Typography variant="h4" color="textPrimary" gutterBottom>
-          Experiência Profissional
-        </Typography>
-        <Typography variant="body1" color="textSecondary" paragraph>
-          <strong>Analista e Desenvolvedor</strong> - NDD
-          <br />
-          <em>Outubro de 2022 - Presente</em>
-          <br />
-          Responsabilidades:
-          <ul>
-            <li>Desenvolvimento de aplicações web utilizando C# e Angular.</li>
-            <li>Design e implementação de APIs RESTful.</li>
-            <li>Colaboração com equipes multidisciplinares para entrega de soluções eficientes.</li>
-          </ul>
-          Conquistas:
-          <ul>
-            <li>Contribuí para a melhoria do processo de Code Review no time, implementando o uso do SonarQube nos projetos.</li>
-          </ul>
-        </Typography>
-        </Section>
-
-        <Section>
-          <Typography variant="h4" color="textPrimary" gutterBottom>
-            Formação Acadêmica
-          </Typography>
-          <Typography variant="body1" color="textSecondary" paragraph>
-            <strong>Graduação em Ciência da Computação</strong> - IFSC
-            <br />
-            <em>2020 - 2024</em>
-            <br />
-          </Typography>
-        </Section>
-
-        <Section>
-          <Typography variant="h4" color="textPrimary" gutterBottom>
-            Cursos Relevantes
-          </Typography>
-          <Typography variant="body1" color="textSecondary" paragraph>
-            <strong>Academia do Programador</strong>
-            <br />
-            <em>2021 - 2022</em>
-            <br />
-          </Typography>
-        </Section>
-
+    <AllContainer>
+      <PrintContent ref={printContentRef} />
+      <Container maxWidth="md" >
         <Button
           variant="contained"
           color="primary"
           startIcon={<DownloadIcon />}
-          onClick={handleDownload}
+          onClick={handlePrint}
         >
           Baixar Currículo
         </Button>
       </Container>
-    </StyledResume>
+    </AllContainer>
   );
 }
 
