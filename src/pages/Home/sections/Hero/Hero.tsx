939e5e5d9509b25b90a0fa43e6241ae60146ba85
Tales Reig Cordova <talesreig03@gmail.com>
2024-08-19 00:11:20 -0300

    com curriculo estático

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,15 +1,12 @@
 import { Box, Container, Grid, Typography, styled } from "@mui/material";
-import { Link } from 'react-router-dom';
 import ImagePerfil from "../../../../assets/images/imagePerfil.jpeg";
 import DownloadIcon from '@mui/icons-material/Download';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import StyledButton from "../../../../components/StyledButtom/StyledButtom";
+import FileDownloader from "../../../../utils/FileDownloader";
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import jsPDF from "jspdf";
-import html2canvas from "html2canvas";
-import PrintContent from "../Resume/PrintContent";
-import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
 
 function Hero() {
 
@@ -32,38 +29,9 @@ function Hero() {
     border: `1px solid ${theme.palette.primary.contrastText}`
   }));
 
-  const handlePrint = () => {
-    const printContent = document.createElement('div');
-    document.body.appendChild(printContent);
-
-    ReactDOM.render(<PrintContent />, printContent);
-
-    html2canvas(printContent).then((canvas) => {
-      const imgData = canvas.toDataURL("image/png");
-      const pdf = new jsPDF();
-      const imgWidth = 210; // A4 width in mm
-      const pageHeight = 295; // A4 height in mm
-      const imgHeight = (canvas.height * imgWidth) / canvas.width;
-      let heightLeft = imgHeight;
-
-      let position = 0;
-
-      pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
-      heightLeft -= pageHeight;
-
-      while (heightLeft >= 0) {
-        position -= pageHeight;
-        pdf.addPage();
-        pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
-        heightLeft -= pageHeight;
-      }
-
-      pdf.save("Curriculo_Tales_Reig.pdf");
-
-      // Cleanup
-      ReactDOM.unmountComponentAtNode(printContent);
-      document.body.removeChild(printContent);
-    });
+  const handleDownloadCV = () => {
+    const downloader = new FileDownloader("/my-portfolio/Curriculo.pdf", "Currículo_Tales_Reig.pdf");
+    downloader.download();
   };
 
   return (
@@ -79,11 +47,11 @@ function Hero() {
               </Box>
             </Grid>
             <Grid item xs={12} md={7}>
-              <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Tales Reig</Typography>
-              <Typography color="primary.contrastText" variant="h2" textAlign="center" >Analista e Desenvolvedor de Software</Typography>
+            <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Tales Reig</Typography>
+            <Typography color="primary.contrastText" variant="h2" textAlign="center" >Analista e Desenvolvedor de Software</Typography>
               <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
                 <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                  <StyledButton onClick={handlePrint}>
+                  <StyledButton onClick={handleDownloadCV}>
                     <DownloadIcon />
                     <Typography>
                       Download CV
